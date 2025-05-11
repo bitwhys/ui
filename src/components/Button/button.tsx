9642319d21b1532f050926d8d34a2850f35aa670
George Bockari <bit@revisioned.consulting>
2025-05-11 04:40:13 -0400

    feat(button): update button styles and introduce control size tokens

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -9,31 +9,31 @@ import { scheme } from '@/components/Button/button.styles.ts'
 const baseButton = tv({
   base: [
     // Base
-    'relative isolate inline-flex items-baseline justify-center gap-x-2 rounded-[10px] border text-base/6 font-medium',
-    // TODO: move Sizing to variant (small, medium, large)
-    'px-[calc(--spacing(3.5)-1px)] py-[calc(--spacing(2.5)-1px)] sm:px-[calc(--spacing(3)-1px)] sm:py-[calc(--spacing(1.5)-1px)] sm:text-sm/6',
-    'select-none',
+    'gap-x-(--control-gap) relative isolate inline-flex select-none items-baseline justify-center',
+    'rounded-[10px] border text-base/6 font-medium',
+    // Sizing
+    'h-(--control-size) leading-(--control-lineHeight) px-[calc(var(--control-paddingInline)-1px)]',
+    'py-[calc(var(--control-paddingBlock)-1px)]',
     // Focus
-    'focus:not-data-focus:outline-hidden data-focus:outline-2 data-focus:outline-offset-2 data-focus:outline-blue-500',
+    'focus:not-data-focus:outline-hidden data-focus:outline-2 data-focus:outline-offset-2',
+    'data-focus:outline-blue-500',
     // Disabled
     'data-disabled:opacity-50',
     // Icon
     // Icon colors
     '*:data-[slot=icon]:text-(--btn-icon) forced-colors:[--btn-icon:ButtonText]',
     // Icon sizing
-    '*:data-[slot=icon]:-mx-0.5 *:data-[slot=icon]:my-0.5 *:data-[slot=icon]:size-5 *:data-[slot=icon]:shrink-0 *:data-[slot=icon]:self-center sm:*:data-[slot=icon]:my-1 sm:*:data-[slot=icon]:size-4',
+    '*:data-[slot=icon]:-mx-0.5 *:data-[slot=icon]:my-0.5 *:data-[slot=icon]:size-5',
+    '*:data-[slot=icon]:shrink-0 *:data-[slot=icon]:self-center',
+    'sm:*:data-[slot=icon]:my-1 sm:*:data-[slot=icon]:size-4',
   ],
   variants: {
     size: {
-      sm: [
-        // TODO
-      ],
-      md: [
-        // TODO
-      ],
-      lg: [
-        // TODO
-      ],
+      xs: 'px-[calc(var(--control-paddingInline)-1px)] py-[calc(var(--control-paddingBlock)-1px)]',
+      sm: 'px-[calc(var(--control-paddingInline)-1px)] py-[calc(var(--control-paddingBlock)-1px)]',
+      md: 'px-[calc(var(--control-paddingInline)-1px)] py-[calc(var(--control-paddingBlock)-1px)]',
+      lg: 'px-[calc(var(--control-paddingInline)-1px)] py-[calc(var(--control-paddingBlock)-1px)]',
+      xl: 'px-[calc(var(--control-paddingInline)-1px)] py-[calc(var(--control-paddingBlock)-1px)]',
     },
   },
   defaultVariants: {
